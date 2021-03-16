0de2cff5a83323d1549ef5d5732eeac88cb91df6
KDS <kds2048@naver.com>
2021-03-16 15:45:43 +0900

    [03]main=>modified source code

--- workspace/06_oop/src/quiz11/MainClass.java
+++ workspace/06_oop/src/quiz11/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
-		WeekScheduler week = new WeekScheduler(1);
+		WeekScheduler week = new WeekScheduler(1); // 1주차 스케쥴
 		week.run();
 	}
 }
