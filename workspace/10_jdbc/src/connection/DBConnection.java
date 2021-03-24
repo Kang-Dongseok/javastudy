ed6cd0c3dfac648dfec3eb332e0afe17f271aec4
kds <kds2048@naver.com>
2021-03-24 17:22:35 +0900

    update 2021-03-24

---
+++ workspace/10_jdbc/src/connection/DBConnection.java
@@ -0,0 +1,30 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+	// method
+	// 1. 결과타입 : Connection
+	// 2. 메소드명 : getConnection
+	// 3. 결과타입 : 없음
+	// 4. 예외처리 : ClassNotFoundException, SQLException
+	
+	// throws의 의미
+	// getConnection() 메소드에서는 예외처리를 하지 않고,
+	// getConnection() 메소드를 호출하는 곳으로 예외를 던져서 처리하겠다.
+	
+	// public Connection getConnection() throws ClassNotFoundException, SQLException{
+	public static Connection getConnection() throws Exception{
+		Class.forName("oracle.jdbc.driver.OracleDriver"); // OracleDriver 파일을 메모리에 올리는 작업
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+		String user = "spring";
+		String password = "1111";
+		Connection con = DriverManager.getConnection(url, user, password);
+		return con;
+		
+	}
+	
+}
